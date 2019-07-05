f4f81ff55095ac72ee7691eb57020a63ec2518f7
Student <jsitbon@qcc.cuny.edu>
2019-07-05 17:19:18 -0400

    updated JSX code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  increment = () => {
+    // implement our state changeto increment the count;
+    this.setState({
+      counter: this.state.counter +1
+    });
+  }
+
+
+
+  render() {
+    console.log("This is line 13 representating state", this.state.counter);
+    return (
+      <div className="container">
+        <div className="navbar">Counter.js</div>
+        <div className="counter">
+          <h1> {this.state.counter} </h1>
+          <button type="button">Increment</button>
+          <button type="button">Decrement</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default Counter;
